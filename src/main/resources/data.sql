757cbbbd1dea782767d85195bc022d8d174b25a8
Adrián Núñez <adrinunezmunoz@gmail.com>
2021-04-28 13:05:45 +0200

    Fixed filters and changed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,11 @@
 
 
-    insert into expert(nombre,created_at,updated_at,estado_motivo,disponibilidad,modalidad,autonomo,contacto_telefono,contacto_email,condiciones_porcentaje,condiciones_precio_hora,puntuacion,nif,credenciales_correo, credenciales_correo_password,credenciales_zoom_password,fichero_foto,fichero_cv,observaciones,origen,estado) values ("Adri",null,"2021-04-14 07:11:41",null,"mañanas",null,null,"12313423","email@email.com","10.00","15.00",70,"123412341A","Hola", "", "",null,null,null,null,"casado");
+    insert into expert(nombre,created_at,updated_at,estado_motivo,disponibilidad,modalidad,autonomo,contacto_telefono,contacto_email,condiciones_porcentaje,condiciones_precio_hora,puntuacion,nif,credenciales_correo, credenciales_correo_password,credenciales_zoom_password,fichero_foto,fichero_cv,observaciones,origen,estado) values ("Adri",null,"2021-04-14 07:11:41","null","mañanas","modo1",null,"12313423","email@email.com","10.00","15.00",70,"123412341A","Hola", "", "",null,null,null,null,"pendiente");
+    insert into expert(nombre,created_at,updated_at,estado_motivo,disponibilidad,modalidad,autonomo,contacto_telefono,contacto_email,condiciones_porcentaje,condiciones_precio_hora,puntuacion,nif,credenciales_correo, credenciales_correo_password,credenciales_zoom_password,fichero_foto,fichero_cv,observaciones,origen,estado) values ("Adricc",null,"2021-04-14 07:11:41",null,"mañanas","moda1",null,"12313423","email@email.com","10.00","15.00",70,"123412341A","Hola", "", "",null,null,null,null,"disponible");
+    insert into expert(nombre,created_at,updated_at,estado_motivo,disponibilidad,modalidad,autonomo,contacto_telefono,contacto_email,condiciones_porcentaje,condiciones_precio_hora,puntuacion,nif,credenciales_correo, credenciales_correo_password,credenciales_zoom_password,fichero_foto,fichero_cv,observaciones,origen,estado) values ("Adrixx",null,"2021-04-14 07:11:41",null,"mañanas","moda2",null,"12313423","email@email.com","10.00","15.00",70,"123412341A","Hola", "", "",null,null,null,null,"pendiente");
+
+
+
 
     insert into tag(nombre,created_at,updated_at) values ("Java","2021-04-06 08:22:59","2021-04-06 08:22:59");
     insert into tag(nombre,created_at,updated_at) values ("Kubernates","2021-04-06 08:22:59","2021-04-06 08:22:59");
