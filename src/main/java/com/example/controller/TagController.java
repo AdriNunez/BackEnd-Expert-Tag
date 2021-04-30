6733a0ba3885ca0a4896fd4d66d6244af9faaa62
Adrián Núñez <adrinunezmunoz@gmail.com>
2021-04-30 17:55:15 +0200

    Fixed minor bugs

--- src/main/java/com/example/controller/TagController.java
+++ src/main/java/com/example/controller/TagController.java
@@ -28,7 +28,7 @@ public class TagController {
 //    }
 
     @GetMapping("/etiquetas")
-    public List<Tag> retrieveByFilter(@RequestParam(name="limite", required=false, defaultValue = "9") Integer limite,
+    public List<Tag> retrieveByFilter(@RequestParam(name="limite", required=false) Integer limite,
                                          @RequestParam(name="pagina", required=false, defaultValue = "0") Integer pagina,
                                          @RequestParam(name="nombre", required=false) String nombre ,
                                         @RequestParam(name="creador",required =false) String creador){
